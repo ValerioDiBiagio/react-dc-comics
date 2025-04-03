6df328b32b014df3339721203377dcc8e6827397
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-03 19:44:36 +0200

    feat: add css footer menu

--- src/components/footer-components/Footer-menu.jsx
+++ src/components/footer-components/Footer-menu.jsx
@@ -1,3 +1,4 @@
+// importare le immagini
 import digitalComics from "../../assets/img/buy-comics-digital-comics.png"
 import dcMerchandise from "../../assets/img/buy-comics-merchandise.png"
 import subscriptions from "../../assets/img/buy-comics-subscriptions.png"
@@ -7,24 +8,28 @@ import dcPowerVisa from "../../assets/img/buy-dc-power-visa.svg"
 
 // funzione Footer Menù
 export default function FooterMenu() {
-    return (<nav className="footer-menu">
-        <ul>
+    return (<nav>
+        <ul className="footer-menu">
             <li>
-                <img src={digitalComics} alt="digital-comics" />
-                Digital Comics
+                <img className="footermenuimg" src={digitalComics} alt="digital-comics" />
+                <div className="footer-nav-text">Digital Comics</div>
             </li>
             <li>
-                <img src={dcMerchandise} alt="dc-merchandise" />
-                DC Merchandise</li>
+                <img className="footermenuimg" src={dcMerchandise} alt="dc-merchandise" />
+                <div className="footer-nav-text">DC Merchandise</div>
+            </li>
             <li>
-                <img src={subscriptions} alt="subscriptions" />
-                Subscription</li>
+                <img className="footermenuimg" src={subscriptions} alt="subscriptions" />
+                <div className="footer-nav-text">Subscription</div>
+            </li>
             <li>
-                <img src={shopLocator} alt="shop-locator" />
-                Comic Shop Locator</li>
+                <img className="footermenuimg" src={shopLocator} alt="shop-locator" />
+                <div className="footer-nav-text">Comic Shop Locator</div>
+            </li>
             <li>
-                <img src={dcPowerVisa} alt="dc-power-visa" />
-                DC Power Visa</li>
+                <img className="footermenuimgsvg" src={dcPowerVisa} alt="dc-power-visa" />
+                <div className="footer-nav-text">DC Power Visa</div>
+            </li>
         </ul>
     </nav>);
 }
