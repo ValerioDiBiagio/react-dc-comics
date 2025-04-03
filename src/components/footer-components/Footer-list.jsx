73fba689be5d488200021dbf8a9872ff4e320fca
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-03 21:58:16 +0200

    feat: add css footer list

--- src/components/footer-components/Footer-list.jsx
+++ src/components/footer-components/Footer-list.jsx
@@ -1,48 +1,50 @@
 // funzione Footer List
 export default function FooterList() {
-    return (<div className="row">
-        <div className="column">
-            <ul>
-                <h3>DC Comics</h3>
-                <li>Characters</li>
-                <li>Comics</li>
-                <li>Movies</li>
-                <li>TV</li>
-                <li>Games</li>
-                <li>Videos</li>
-                <li>News</li>
-            </ul>
+    return (<div className="footerlist">
+        <div className="row">
+            <div className="column">
+                <ul>
+                    <h3>DC Comics</h3>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
 
-            <ul>
-                <h3>Shop</h3>
-                <li>Shop DC</li>
-                <li>Shop DC Collectibles</li>
-            </ul>
-        </div>
-        <div className="column">
-            <ul>
-                <h3>DC</h3>
-                <li>Terms of use</li>
-                <li>Privacy policy(New)</li>
-                <li>Ad Choices</li>
-                <li>Advertising</li>
-                <li>Subscriptions</li>
-                <li>Talent Workshops</li>
-                <li>CPSC Certificates</li>
-                <li>Ratings</li>
-                <li>Shop Help</li>
-                <li>Contact Us</li>
-            </ul>
-        </div>
-        <div className="column">
-            <ul>
-                <h3>Sites</h3>
-                <li>DC</li>
-                <li>MAD Megazine</li>
-                <li>DC Kids</li>
-                <li>DC Universe</li>
-                <li>DC Power Visa</li>
-            </ul>
+                <ul>
+                    <h3>Shop</h3>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
+            <div className="column">
+                <ul>
+                    <h3>DC</h3>
+                    <li>Terms of use</li>
+                    <li>Privacy policy(New)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div className="column">
+                <ul>
+                    <h3>Sites</h3>
+                    <li>DC</li>
+                    <li>MAD Megazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
         </div>
     </div>);
 }
