eb09f36bc0e39d2e9ed327064e7ca28fe7fd5659
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-03 20:40:21 +0200

    feat: add footer list structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 
+import FooterList from "./footer-components/Footer-list";
 import FooterMenu from "./footer-components/Footer-menu";
+
 
 
 // funzione Footer
 export default function Footer() {
     return (<footer>
         <FooterMenu />
+        <FooterList />
     </footer>);
 }
