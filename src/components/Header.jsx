0a1a6022cb5e25e9cc6cae620244aba5476ade7f
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-03 16:57:44 +0200

    feat: add header structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,26 @@
+
+// importare il logo
+import logo from "../assets/img/dc-logo.png";
+
 // funzione Header
 export default function Header() {
-    return (<header> Intestazione </header>);
+    return (<header>
+        <img src={logo} alt="logo-dc" />
+        <div>
+            <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Movies</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+            </ul>
+        </div>
+    </header>);
 }
+
+
