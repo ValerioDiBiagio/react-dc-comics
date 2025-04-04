7b84a1715e881a9f6a75e9358d191ae71a623a69
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-04 17:37:15 +0200

    feat: add props in header nav

--- src/components/Header-components/Header-nav.jsx
+++ src/components/Header-components/Header-nav.jsx
@@ -1,17 +1,23 @@
+// importare funzione per i link della nav
+import ArrayNav from "./Nav-props";
+
 // funzione nav
 export default function HeaderNav() {
+
+    const arrayNav = ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Collectibles', 'Videos', 'Fans', 'News', 'Shop'];
+
     return (
         <ul>
-            <li className="hovereffect">Characters</li>
-            <li className="hovereffect">Comics</li>
-            <li className="hovereffect">Movies</li>
-            <li className="hovereffect">TV</li>
-            <li className="hovereffect">Games</li>
-            <li className="hovereffect">Collectibles</li>
-            <li className="hovereffect">Videos</li>
-            <li className="hovereffect">Fans</li>
-            <li className="hovereffect">News</li>
-            <li className="hovereffect">Shop</li>
+            <ArrayNav title={arrayNav[0]} />
+            <ArrayNav title={arrayNav[1]} />
+            <ArrayNav title={arrayNav[2]} />
+            <ArrayNav title={arrayNav[3]} />
+            <ArrayNav title={arrayNav[4]} />
+            <ArrayNav title={arrayNav[5]} />
+            <ArrayNav title={arrayNav[6]} />
+            <ArrayNav title={arrayNav[7]} />
+            <ArrayNav title={arrayNav[8]} />
+            <ArrayNav title={arrayNav[9]} />
         </ul>
     )
 }
---
+++ src/components/Header-components/Nav-props.jsx
@@ -0,0 +1,6 @@
+// funzione per link della nav
+export default function ArrayNav(props) {
+    return (
+        <li className="hovereffect"><a href="#">{props.title}</a></li>
+    )
+}
