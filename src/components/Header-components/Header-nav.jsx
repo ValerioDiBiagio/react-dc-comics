d1db2aa1870b5ae6b7695fe2c0b084ceb670808b
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-04 19:09:42 +0200

    feat: fix props header

--- src/components/Header-components/Header-nav.jsx
+++ src/components/Header-components/Header-nav.jsx
@@ -2,22 +2,19 @@
 import ArrayNav from "./Nav-props";
 
 // funzione nav
-export default function HeaderNav() {
-
-    const arrayNav = ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Collectibles', 'Videos', 'Fans', 'News', 'Shop'];
-
+export default function HeaderNav(props) {
     return (
         <ul>
-            <ArrayNav title={arrayNav[0]} />
-            <ArrayNav title={arrayNav[1]} />
-            <ArrayNav title={arrayNav[2]} />
-            <ArrayNav title={arrayNav[3]} />
-            <ArrayNav title={arrayNav[4]} />
-            <ArrayNav title={arrayNav[5]} />
-            <ArrayNav title={arrayNav[6]} />
-            <ArrayNav title={arrayNav[7]} />
-            <ArrayNav title={arrayNav[8]} />
-            <ArrayNav title={arrayNav[9]} />
+            <ArrayNav title={props.linkNav[0]} />
+            <ArrayNav title={props.linkNav[1]} />
+            <ArrayNav title={props.linkNav[2]} />
+            <ArrayNav title={props.linkNav[3]} />
+            <ArrayNav title={props.linkNav[4]} />
+            <ArrayNav title={props.linkNav[5]} />
+            <ArrayNav title={props.linkNav[6]} />
+            <ArrayNav title={props.linkNav[7]} />
+            <ArrayNav title={props.linkNav[8]} />
+            <ArrayNav title={props.linkNav[9]} />
         </ul>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
-
 // importare componente logo
 import Logo from "./Header-components/Header-logo";
 
 // imnportare componente nav
 import HeaderNav from "./Header-components/Header-nav";
 
-// funzione Header
+// creazione dell'array di link
+const arrayNav = ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Collectibles', 'Videos', 'Fans', 'News', 'Shop'];
+
 export default function Header() {
     return (<header>
         <nav>
             <Logo />
-            <HeaderNav />
+            <HeaderNav linkNav={arrayNav} />
         </nav>
     </header>);
 }
